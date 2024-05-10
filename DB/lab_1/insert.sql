e4fc3fc219ee8677c6e295e09ab8c80b646ab91a
vanya9090 <ivanmironov27112005@gmail.com>
2024-05-10 15:24:33 +0300

    add trigger to insert script

--- DB/lab_1/insert.sql
+++ DB/lab_1/insert.sql
@@ -35,3 +35,21 @@ INSERT INTO used_result (task_id, result_id) VALUES
 (1, 2);
 
 SELECT pg_typeof(location.latitude) from location;
+
+CREATE OR REPLACE FUNCTION check_task_date()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.start_date < (SELECT start_date FROM task WHERE id = NEW.task_id) THEN
+    RAISE EXCEPTION 'start_date in scientist_task cannot by earlier than start_date in task';
+  END IF;
+  IF NEW.end_date > (SELECT end_date FROM task WHERE id = NEW.task_id) THEN
+    RAISE EXCEPTION 'end_date in scientist_task cannot by later than end_date in task';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER task_date_trigger
+BEFORE INSERT ON scientist_task
+FOR EACH ROW
+EXECUTE PROCEDURE check_task_date();
